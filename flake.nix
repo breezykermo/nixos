5a814c5c27ed7b77d9dd73c4a65fad8211074371
Lachlan Kermode <lachlankermode@live.com>
2023-11-04 21:36:26 +0100

    updating

--- flake.nix
+++ flake.nix
@@ -5,6 +5,12 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+    raw-browser = {
+      url = "sourcehut:~nomisiv/raw-browser";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    diosevka.url = "sourcehut:~nomisiv/diosevka";
 	};
 
 	outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -15,8 +21,8 @@
 					./nixos/configuration.nix
 						home-manager.nixosModules.home-manager
 						{
-							home-manager.useGlobalPkgs = true;
-							home-manager.useUserPackages = true;
+							# home-manager.useGlobalPkgs = true;
+							# home-manager.useUserPackages = true;
 							home-manager.users.alice = import ./home-manager/home.nix;
 						}
 				];
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,21 +1,34 @@
 { ... }:
 
+
 {
 	imports = [
 		./server
 		./desktop
 	];
+
+	gui = {
+		enable = true;
+
+		monitor = {
+			name = "eDP-1";
+			height = 1440;
+			width = 2560;
+			scale = 1.5;
+			touch = true;
+		};
+
+		waybar.modules = {
+			label = "simon@x1carbon";
+			cpu.temperaturePath = "/sys/class/hwmon/hwmon4/temp1_input";
+		};
+
+		laptop = true;
+	};
+
 	home = {
 		username = "alice";
 		homeDirectory = "/home/alice";
-	};
-
-	xdg = {
-		userDirs = {
-			enable = true;
-			desktop = "$HOME/.desktop";
-			download = "$HOME/downloads";
-		};
 	};
 
 	home.stateVersion = "23.05";
