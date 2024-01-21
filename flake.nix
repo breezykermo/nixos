de267fb7231e98b0f470bab35b41377756fd55b5
Lachlan Kermode <lachlan_kermode@brown.edu>
2024-01-21 22:33:48 +1300

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
-    diosevka.url = "sourcehut:~nomisiv/diosevka";
+		hyprland.url = "github:hyprwm/Hyprland";
+		diosevka.url = "sourcehut:~nomisiv/diosevka";
 	};
 
 	outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -16,18 +16,17 @@
 				modules = [
 					./nixos/configuration.nix
 					home-manager.nixosModules.home-manager
-          # hyprland.nixosModules.default
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              sharedModules = [
-                {home.stateVersion = "23.05";}
-                ./home-manager
-                # hyprland.homeManagerModules.default
-              ];
-            };
-          }
+					# hyprland.nixosModules.default
+					{
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							sharedModules = [
+								{home.stateVersion = "23.05";}
+								./home-manager
+							];
+						};
+					}
 					./machines/thinkpad.nix
 				];
 			};
