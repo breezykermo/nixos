11e9636f0039d8de3cb05f114f744a0eb10cf408
Lachlan Kermode <lachlankermode@live.com>
2024-01-22 02:50:05 +0100

    Add alacritty colors

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -26,11 +26,43 @@
         normal.family = "Liberation Mono";
         size = 14;
       };
+      window.decorations = "none";
+      scrolling.history = 0;
       shell = {
         program = "${pkgs.fish}/bin/fish";
         args = [ "--interactive" ];
       };
-      draw_bold_text_with_bright_colors = true;
+      colors = {
+        draw_bold_text_with_bright_colors = true;
+        primary = {
+          background = "0x1d2021";
+          foreground = "0xd5c4a1";
+        };
+        cursor = {
+          text = "0x1d2021";
+          cursor = "0xd5c4a1";
+        };
+        bright = {
+          black =   "0x665c54";
+          red =     "0xfe8019";
+          green =   "0x3c3836";
+          yellow =  "0x504945";
+          blue =    "0xbdae93";
+          magenta = "0xebdbb2";
+          cyan =    "0xd65d0e";
+          white =   "0xfbf1c7";
+        };
+        normal = {
+          black =   "0x1d2021";
+          red =     "0xfb4934";
+          green =   "0xb8bb26";
+          yellow =  "0xfabd2f";
+          blue =    "0x83a598";
+          magenta = "0xd3869b";
+          cyan =    "0x8ec07c";
+          white =   "0xd5c4a3";
+        };
+      };
     };
   };
 
