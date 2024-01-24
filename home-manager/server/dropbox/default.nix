2c82ed2920b4d5bc58d9a22a69e5522a3c1b96ce
Lachlan Kermode <lachlankermode@live.com>
2024-01-24 07:29:35 +0100

    ++

--- home-manager/server/dropbox/default.nix
+++ home-manager/server/dropbox/default.nix
@@ -1,5 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-	services.dropbox.enable = true;
+	systemd.user.services.dropbox = {
+		Description = "Dropbox";
+		WantedBy = [ "graphical-session.target" ];
+		Environment = {
+			QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+			QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+		};
+		ServiceConfig = {
+			ExecStart = "${lib.getBin pkgs.dropbox}/bin/dropbox";
+			ExecReload = "${lib.getBin pkgs.coreutils}/bin/kill -HUP $MAINPID";
+			KillMode = "control-group"; # upstream recommends process
+				Restart = "on-failure";
+			PrivateTmp = true;
+			ProtectSystem = "full";
+			Nice = 10;
+		};
+	};
 }
--- home-manager/server/emacs/default.nix
+++ home-manager/server/emacs/default.nix
@@ -1,13 +1,14 @@
 { lib, pkgs, ... }:
 {
-  environment.sessionVariables = rec {
-   DOOMDIR = "$HOME/nixos-config/home-manager/server/emacs/doom.d";
-  };
-
   programs.emacs = {
     enable = true; 
     package = pkgs.emacs;
     # extraConfig = builtins.readFile ./config.el;
+  };
+
+  home.file.".doom.d" = {
+    source = ./doom.d;
+    recursive = true;
   };
 
 # https://github.com/nix-community/nix-doom-emacs is broken ATM.
@@ -15,8 +16,11 @@
 # but currently I don't have a better way to automate this: so for each fresh install:
 # so just install doom manually.
 
+
 #   git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 #   ~/.config/emacs/bin/doom install 
 #   ~/.config/emacs/bin/doom sync
 
+# See https://github.com/yrashk/nix-home/blob/master/home.nix#L379-L389
+# for an example of how to just git clone a doomemacs revision automatically.
 }
