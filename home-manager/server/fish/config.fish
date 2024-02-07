d7ce07b141fa5b34e42b3e1cd3290f1093bd1e46
Lachlan Kermode <lachlankermode@live.com>
2024-02-07 08:03:16 +0100

    Add function to disable janky thinkpad hardware

--- home-manager/server/fish/config.fish
+++ home-manager/server/fish/config.fish
@@ -5,3 +5,9 @@ zoxide init fish | source
 # Necessary hack for environment variables
 # See: https://github.com/nix-community/home-manager/issues/1011
 set -gx DOOMDIR "$HOME/.doom.d"
+
+# Hack for Thinkpad. TODO how do I localise this?
+func disable_touchpad
+	hyprctl keyword "device:tpps/2-elan-trackpoint:enabled" false
+	hyprctl keyword "device:synaptics-tm3289-021:enabled" false
+end
