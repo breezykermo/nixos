fa4d215905568628156e9a9cb683c484b2aaf349
Lachlan Kermode <lachlankermode@live.com>
2024-02-09 07:56:47 +0100

    ++

--- home-manager/server/fish/config.fish
+++ home-manager/server/fish/config.fish
@@ -7,7 +7,7 @@ zoxide init fish | source
 set -gx DOOMDIR "$HOME/.doom.d"
 
 # Hack for Thinkpad. TODO how do I localise this?
-func disable_touchpad
-	hyprctl keyword "device:tpps/2-elan-trackpoint:enabled" false
-	hyprctl keyword "device:synaptics-tm3289-021:enabled" false
-end
+# func disable_touchpad
+# 	hyprctl keyword "device:tpps/2-elan-trackpoint:enabled" false
+# 	hyprctl keyword "device:synaptics-tm3289-021:enabled" false
+# end
