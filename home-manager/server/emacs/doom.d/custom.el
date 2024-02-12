a10cd3e749c64f87e66c6183243e74bfbcd53904
Lachlan Kermode <lachlankermode@live.com>
2024-02-12 07:46:13 +0100

    better

--- home-manager/server/emacs/doom.d/custom.el
+++ home-manager/server/emacs/doom.d/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-    ;; TODO work out how to propagate the `basep` variable here
-   '(org-agenda-files (directory-files-recursively "$HOME/Dropbox (Brown)/lyt/org/" "\\.org$")))
+ '(org-agenda-files (directory-files-recursively "$HOME/Dropbox (Brown)/lyt/org/" "\\.org$"))
  '(package-selected-packages '(pbcopy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
