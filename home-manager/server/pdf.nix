7ec951ee9f85572f34520356f399378401a2420b
Lachlan Kermode <lachlankermode@live.com>
2024-02-12 10:11:18 +0100

    g

--- home-manager/server/pdf.nix
+++ home-manager/server/pdf.nix
@@ -3,12 +3,13 @@
 	home.packages = with pkgs; [
 		# tectonic
 		# biber-for-tectonic
+		texlive.combined.scheme-basic
 	];
 
 	programs = {
 		# see https://github.com/nix-community/home-manager/blob/master/modules/programs/texlive.nix
-		texlive = {
-			enable = true;
-		};
+		# texlive = {
+		# 	enable = true;
+		# };
 	};
 }
