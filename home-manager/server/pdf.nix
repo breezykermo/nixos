d77ad52aad57f83d1c5b09ce618502706825f89f
Lachlan Kermode <lachlankermode@live.com>
2024-02-12 10:27:52 +0100

    g

--- home-manager/server/pdf.nix
+++ home-manager/server/pdf.nix
@@ -4,7 +4,7 @@
 		# XXX trying to switch to tectonic, but haven't yet worked out the correct way to do citations in Org Mode.
 		# tectonic
 		# biber-for-tectonic
-		texlive.combined.scheme-basic
+		inherit (texlive) scheme-minimal latex-bin latexmk
 	];
 
 	programs = {
