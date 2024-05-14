84271afb25445597226b4a6c415172b22a21bc60
lox <lox@ohrg.org>
2024-05-14 20:56:05 +0200

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,7 +12,7 @@
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-	networking.hostName = "nixos";
+	networking.hostName = "loxnix";
 	networking.networkmanager.enable = true;
 
 	# Set your time zone.
