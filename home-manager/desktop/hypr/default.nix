73e44f9bc806897eaa96c5c2e1e3545338435f4d
lox <lox@ohrg.org>
2024-05-18 17:11:53 +0200

    changes

--- home-manager/desktop/hypr/default.nix
+++ home-manager/desktop/hypr/default.nix
@@ -1,8 +1,23 @@
 { ... }:
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    extraConfig = builtins.readFile ./hypr.conf;
-  };
+	wayland.windowManager.hyprland = {
+		enable = true;
+		extraConfig = builtins.readFile ./hypr.conf;
+	};
+
+	services.hyprpaper = {
+		enable = true;
+		settings = {
+			ipc = "on";
+			splash = "true";
+
+			preload = [];
+
+			wallpaper = [
+				"DP-3,/share/wallpapers/buttons.png"
+				"eDP-1,/home/alice/Dropbox (Brown)/data/wallpapers/bike-wallpaper.jpg"
+			];
+		}; 
+	}
 }
  
