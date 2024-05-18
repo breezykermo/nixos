6cafbb60f488feb54c3a0910f3a10c9147548943
lox <lox@ohrg.org>
2024-05-18 17:12:22 +0200

    changes

--- home-manager/desktop/hypr/default.nix
+++ home-manager/desktop/hypr/default.nix
@@ -14,10 +14,9 @@
 			preload = [];
 
 			wallpaper = [
-				"DP-3,/share/wallpapers/buttons.png"
 				"eDP-1,/home/alice/Dropbox (Brown)/data/wallpapers/bike-wallpaper.jpg"
 			];
 		}; 
-	}
+	};
 }
  
