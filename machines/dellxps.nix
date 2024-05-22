57736231f8cfac399b7170bf796621fc2e605c74
Lachlan Kermode <lachlankermode@live.com>
2024-05-22 10:56:47 +0200

    battery improvements

--- machines/dellxps.nix
+++ machines/dellxps.nix
@@ -29,9 +29,21 @@
 		};
 
 	};
-
-# necessary for swaylock
-# security.pam.services.swaylock = {};
+	# improve battery life 
+	# https://github.com/TechsupportOnHold/Batterylife/blob/main/laptop.nix
+	services.system76-scheduler.settings.cfsProfiles.enable = true;
+	services.thermald.enable = true;
+	powerManager.powertop.enable = true;
+	services.power-profiles-daemon.enable = false;
+	services.tlp = {
+		enable = true;
+		settings = {
+			CPU_BOOST_ON_AC = 1;
+			CPU_BOOST_ON_BAT = 0;
+			CPU_SCALING_GOVERNOR_ON_AC = "performance";
+			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+		};
+	};
 
 	# necessary for sway
 	security.polkit.enable = true;
