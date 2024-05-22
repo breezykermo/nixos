3ef9f9c20a5fd29e78e844cc0593cfa0c303d8d9
Lachlan Kermode <lachlankermode@live.com>
2024-05-22 11:15:38 +0200

    add swaybg

--- home-manager/desktop/hypr/default.nix
+++ home-manager/desktop/hypr/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
 	home.packages = with pkgs; [
-		# hyprpaper
 		swaybg
 	];
 
@@ -11,20 +10,5 @@
 	};
 
 	# home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
-
-# NB: only available in unstable home manager, as of 2024.05.18
-#services.hyprpaper = {
-#	enable = true;
-#	settings = {
-#		ipc = "on";
-#		splash = "true";
-
-#		preload = [];
-
-#		wallpaper = [
-#			"eDP-1,/home/alice/Dropbox (Brown)/data/wallpapers/bike-wallpaper.jpg"
-#		];
-#	}; 
-#};
 }
  
