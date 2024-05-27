9f350a8c631e253a152248da7a49d02ad641ff49
Lachlan Kermode <lachlankermode@live.com>
2024-05-27 15:05:43 +0200

    optimize storage

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,17 +43,23 @@
 		curl
 	];
 
-	# environment.variables.EDITOR = "vi";
+	# Limit the number of generations to keep
+  boot.loader.systemd-boot.configurationLimit = 10;
+  # boot.loader.grub.configurationLimit = 10;
 
-	# services.openssh = {
-	# 	enable = true;
-	# 	settings = {
-	# 		X11Forwarding = true;
-	# 		PermitRootLogin = "yes";
-	# 		PasswordAuthentication = false;
-	# 	};
-	# 	openFirewall = true;
-	# };
+  # Perform garbage collection weekly to maintain low disk usage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
 
 	# This value determines the NixOS release from which the default
 	# settings for stateful data, like file locations and database versions
