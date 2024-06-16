c15e02f3aef7743019632463694b31c52c20ddbc
Lachlan Kermode <lachlankermode@live.com>
2024-06-16 11:14:02 +0200

    up

--- home-manager/server/neovim/init.lua
+++ home-manager/server/neovim/init.lua
@@ -436,25 +436,6 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>f', function()
-	-- You can pass additional configuration to telescope to change theme, layout, etc.
-	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-		winblend = 10,
-		previewer = false,
-	})
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<c-p>', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>ss', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-
 require('which-key').register {
 	['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
 	['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
@@ -467,10 +448,30 @@ require('which-key').register {
 
 -- General opts for remaps
 local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+
+-- Search in codebase, see `:help telescope.builtin`
+map('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+map('n', '<leader>f', function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+map('n', '<c-p>', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+map('n', '<leader>ss', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+map('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+map('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+map('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+map('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+map('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+map('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- NERDTree
-vim.keymap.set('n', '<leader>n', ':NERDTreeFind<cr>', opts)
-vim.keymap.set('n', '<leader>k', ':NERDTreeToggle<cr>', opts)
+map('n', '<leader>k', ':NERDTreeFind<cr>', opts)
+map('n', '<leader>m', ':NERDTreeToggle<cr>', opts)
 
 -- Tabs, see https://github.com/romgrk/barbar.nvim
 map('n', '<C-z>', '<Cmd>BufferPrevious<CR>', opts)
