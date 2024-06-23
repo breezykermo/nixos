1cbb68dd727c44ee1a1dcc6d8917259b6878ce13
Lachlan Kermode <lachlankermode@live.com>
2024-06-23 22:10:10 +0200

    wip

--- machines/dellxps.nix
+++ machines/dellxps.nix
@@ -65,6 +65,7 @@
 	'';
 	environment.systemPackages = [ pkgs.v4l-utils ];
 	# NB this line is needed for reasons described here: https://discourse.nixos.org/t/normal-users-not-appearing-in-login-manager-lists/4619/4
-	environment.shells = with pkgs; [ bashInteractive fish ];
-  users.users.alice.shell = pkgs.fish;
+	environment.shells = with pkgs; [ bashInteractive ];
+ #  users.users.alice.shell = pkgs.fish;
+	# users.users.alice.ignoreShellProgramCheck = true;
 }
