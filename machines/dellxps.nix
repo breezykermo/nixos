66579d2eeee30226e7d78977055dd8d4bf51c605
Lachlan Kermode <lachlankermode@live.com>
2024-06-23 21:15:41 +0200

    wip

--- machines/dellxps.nix
+++ machines/dellxps.nix
@@ -54,4 +54,12 @@
 
 	# bluetooth
 	hardware.bluetooth.enable = true;
+
+	 # https://nixos.wiki/wiki/OBS_Studio
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 }
