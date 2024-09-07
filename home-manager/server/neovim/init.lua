16306ebccb99ea6ee7b36b95cbb0ee26f9a74446
Lachlan Kermode <lachlankermode@live.com>
2024-09-06 21:29:23 -0400

    up

--- home-manager/server/neovim/init.lua
+++ home-manager/server/neovim/init.lua
@@ -203,6 +203,30 @@ require('lazy').setup({
             org_export = '<leader>ae',
             org_insert_link = '<leader>al',
             org_agenda_deadline = '<leader>ad',
+            org_agenda_schedule = '<leader>as',
+          },
+          capture = {
+            org_capture_kill = '<leader>k',
+          },
+        },
+        org_custom_exports = {
+          -- 'f' is the shortcut prompt 
+          f = {
+            label = 'Export to RTF format',
+            action = function(exporter)
+              local current_file = vim.api.nvim_buf_get_name(0)
+              local target = vim.fn.fnamemodify(current_file, ':p:r')..'.rtf'
+              local command = {'pandoc', current_file, '-o', target}
+              local on_success = function(output)
+                print('Success!')
+                vim.api.nvim_echo({{ table.concat(output, '\n') }}, true, {})
+              end
+              local on_error = function(err)
+                print('Error!')
+                vim.api.nvim_echo({{ table.concat(err, '\n'), 'ErrorMsg' }}, true, {})
+              end
+              return exporter(command , target, on_success, on_error)
+            end
           }
         }
       })
