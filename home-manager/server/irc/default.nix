cb47a497503879a248a28c270cca5709eaf3f726
Lachlan Kermode <lachlankermode@live.com>
2024-09-20 08:17:25 -0400

    add weechat w wee-slack

--- home-manager/server/irc/default.nix
+++ home-manager/server/irc/default.nix
@@ -1,8 +1,13 @@
 { config, pkgs, lib, ...}:
+let
+  myWeechat = pkgs.weechat.override {
+    configure = { availablePlugins, ... }: {
+      scripts = with pkgs.weechatScripts; [
+        wee-slack  
+      ];
+    };
+  };
+in
 {
-	home.packages = with pkgs; [
-    weechat
-  ];
-  programs.irssi.enable = true;
+  home.packages = [ myWeechat ];
 }
-
