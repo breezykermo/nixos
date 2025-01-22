1a95479e7de80f4edd98db2f26126000f7aa2bdb
Lachlan Kermode <lachiekermode@gmail.com>
2025-01-22 15:57:58 +0100

    Restructure

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	description = "Lachie's NixOS Flake";
+  description = "Lachie's NixOS Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,36 +10,35 @@
     ghostty = {
       url = "github:ghostty-org/ghostty";
     };
-	};
+  };
 
   outputs = inputs@{
     nixpkgs, 
     home-manager, 
     ... }:
   let 
-		system = "x86_64-linux";
-    userName = "alice";
-	in
-	{
-		nixosConfigurations.loxnix = nixpkgs.lib.nixosSystem {
-			inherit system; 
+    system = "x86_64-linux";
+    userName = "lox";
+  in
+  {
+    nixosConfigurations.loxnix = nixpkgs.lib.nixosSystem {
+      inherit system; 
       specialArgs = { inherit userName; };
-			modules = [
-				./nixos/configuration.nix
+      modules = [
+        ./configuration.nix
+        # ./machines/framework/configuration.nix
+        ./machines/dellxps/configuration.nix
 
-				# Use home-manager to configure different users
-				home-manager.nixosModules.home-manager {
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						extraSpecialArgs = { inherit inputs system userName; };
+        home-manager.nixosModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs system userName; };
 
-						users."${userName}" = import ./home-manager;
-					};
-				}
-
-				./machines/dellxps.nix
-			];
-		};
-	};
+            users."${userName}" = import ./home-manager;
+          };
+        }
+      ];
+    };
+  };
 }
