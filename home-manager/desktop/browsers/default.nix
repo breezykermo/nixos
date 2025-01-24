4d9aeef8f1bded04ea9f806c60b579542c38bd01
Lachlan Kermode <lachiekermode@gmail.com>
2025-01-24 11:24:16 +0100

    up

--- home-manager/desktop/browsers/default.nix
+++ home-manager/desktop/browsers/default.nix
@@ -71,6 +71,15 @@ in
         '';
 
         userChrome = builtins.readFile ./userChrome.css;
+        
+        bookmarks = [
+          {
+            name = "wikipedia";
+            tags = [ "wiki" ];
+            keyword = "wiki";
+            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+          }
+        ];
       };
     };
 
