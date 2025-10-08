7591cbfe1aacc04ba94faa174741e48143608466
Lachlan Kermode <lachie@ohrg.org>
2025-10-08 21:49:26 +0200

    Adds helix

--- home-manager/desktop/browsers/default.nix
+++ home-manager/desktop/browsers/default.nix
@@ -204,7 +204,7 @@ in
       # Valid strings for installation_mode are "allowed", "blocked",
       # "force_installed" and "normal_installed".
       ExtensionSettings = {
-        "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        "*".installation_mode = "allowed"; # blocks all addons except the ones specified below
         # uBlock Origin:
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
@@ -235,6 +235,7 @@ in
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-vpn-firefox-extension/latest.xpi";
           installation_mode = "force_installed";
         };
+        # Zotero
       };
 
       /* ---- PREFERENCES ---- */
