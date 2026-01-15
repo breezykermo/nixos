52a5ffec7599d80e7d13e93c9a95dc510620f734
Lachlan Kermode <lachie@ohrg.org>
2026-01-15 09:27:30 +0100

    Run start-hyprland on login

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/start-hyprland";
         user = "greeter";
       };
     };
