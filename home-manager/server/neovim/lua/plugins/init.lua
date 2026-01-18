35133044228f672feba386705f30285e2f7d3e3a
Lachlan Kermode <lachie@ohrg.org>
2026-01-18 13:46:23 +0100

    Betters markdown rendering

--- home-manager/server/neovim/lua/plugins/init.lua
+++ home-manager/server/neovim/lua/plugins/init.lua
@@ -393,6 +393,235 @@ require('lazy').setup({
       }
     end
   },
+
+  -- Render markdown with nice formatting
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    config = function()
+      require('render-markdown').setup({
+        enabled = true,
+        render_modes = { 'n', 'c', 't' },
+        debounce = 100,
+        preset = 'none',
+        log_level = 'error',
+        log_runtime = false,
+        file_types = { 'markdown' },
+        max_file_size = 10.0,
+        injections = {
+          gitcommit = {
+            enabled = true,
+            query = [[
+              ((message) @injection.content
+                (#set! injection.combined)
+                (#set! injection.include-children)
+                (#set! injection.language "markdown"))
+            ]],
+          },
+        },
+        anti_conceal = {
+          enabled = true,
+          ignore = {
+            code_background = true,
+            sign = true,
+          },
+        },
+        padding = { highlight = 'Normal' },
+        latex = {
+          enabled = true,
+          converter = 'latex2text',
+          highlight = 'RenderMarkdownMath',
+          position = 'above',
+          top_pad = 0,
+          bottom_pad = 0,
+        },
+        on = {
+          attach = function() end,
+        },
+        completions = {
+          lsp = { enabled = true },
+          blink = { enabled = false },
+          coq = { enabled = false },
+        },
+        heading = {
+          enabled = true,
+          sign = true,
+          icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+          position = 'overlay',
+          signs = { '󰫎 ' },
+          width = 'full',
+          left_margin = 0,
+          left_pad = 0,
+          right_pad = 0,
+          min_width = 0,
+          border = false,
+          border_virtual = false,
+          border_prefix = false,
+          above = '▄',
+          below = '▀',
+          backgrounds = {
+            'RenderMarkdownH1Bg',
+            'RenderMarkdownH2Bg',
+            'RenderMarkdownH3Bg',
+            'RenderMarkdownH4Bg',
+            'RenderMarkdownH5Bg',
+            'RenderMarkdownH6Bg',
+          },
+          foregrounds = {
+            'RenderMarkdownH1',
+            'RenderMarkdownH2',
+            'RenderMarkdownH3',
+            'RenderMarkdownH4',
+            'RenderMarkdownH5',
+            'RenderMarkdownH6',
+          },
+        },
+        paragraph = {
+          enabled = true,
+          left_margin = 0,
+          min_width = 0,
+        },
+        code = {
+          enabled = true,
+          sign = true,
+          style = 'full',
+          position = 'left',
+          language_pad = 0,
+          language_name = true,
+          disable_background = { 'diff' },
+          width = 'full',
+          left_margin = 0,
+          left_pad = 0,
+          right_pad = 0,
+          min_width = 0,
+          border = 'thin',
+          above = '▄',
+          below = '▀',
+          highlight = 'RenderMarkdownCode',
+          highlight_inline = 'RenderMarkdownCodeInline',
+        },
+        dash = {
+          enabled = true,
+          icon = '─',
+          width = 'full',
+          highlight = 'RenderMarkdownDash',
+        },
+        bullet = {
+          enabled = true,
+          icons = { '●', '○', '◆', '◇' },
+          left_pad = 0,
+          right_pad = 0,
+          highlight = 'RenderMarkdownBullet',
+        },
+        checkbox = {
+          enabled = true,
+          position = 'inline',
+          unchecked = {
+            icon = '󰄱 ',
+            highlight = 'RenderMarkdownUnchecked',
+          },
+          checked = {
+            icon = '󰱒 ',
+            highlight = 'RenderMarkdownChecked',
+          },
+          custom = {
+            todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo' },
+          },
+        },
+        quote = {
+          enabled = true,
+          icon = '▋',
+          repeat_linebreak = false,
+          highlight = 'RenderMarkdownQuote',
+        },
+        pipe_table = {
+          enabled = true,
+          preset = 'none',
+          style = 'full',
+          cell = 'padded',
+          padding = 1,
+          min_width = 0,
+          border = {
+            '┌', '┬', '┐',
+            '├', '┼', '┤',
+            '└', '┴', '┘',
+            '│', '─',
+          },
+          alignment_indicator = '━',
+          head = 'RenderMarkdownTableHead',
+          row = 'RenderMarkdownTableRow',
+          filler = 'RenderMarkdownTableFill',
+        },
+        callout = {
+          note = { raw = '[!NOTE]', rendered = '󰋽 Note', highlight = 'RenderMarkdownInfo' },
+          tip = { raw = '[!TIP]', rendered = '󰌶 Tip', highlight = 'RenderMarkdownSuccess' },
+          important = { raw = '[!IMPORTANT]', rendered = '󰅾 Important', highlight = 'RenderMarkdownHint' },
+          warning = { raw = '[!WARNING]', rendered = '󰀪 Warning', highlight = 'RenderMarkdownWarn' },
+          caution = { raw = '[!CAUTION]', rendered = '󰳦 Caution', highlight = 'RenderMarkdownError' },
+          abstract = { raw = '[!ABSTRACT]', rendered = '󰨸 Abstract', highlight = 'RenderMarkdownInfo' },
+          summary = { raw = '[!SUMMARY]', rendered = '󰨸 Summary', highlight = 'RenderMarkdownInfo' },
+          tldr = { raw = '[!TLDR]', rendered = '󰨸 Tldr', highlight = 'RenderMarkdownInfo' },
+          info = { raw = '[!INFO]', rendered = '󰋽 Info', highlight = 'RenderMarkdownInfo' },
+          todo = { raw = '[!TODO]', rendered = '󰗡 Todo', highlight = 'RenderMarkdownInfo' },
+          hint = { raw = '[!HINT]', rendered = '󰌶 Hint', highlight = 'RenderMarkdownSuccess' },
+          success = { raw = '[!SUCCESS]', rendered = '󰄬 Success', highlight = 'RenderMarkdownSuccess' },
+          check = { raw = '[!CHECK]', rendered = '󰄬 Check', highlight = 'RenderMarkdownSuccess' },
+          done = { raw = '[!DONE]', rendered = '󰄬 Done', highlight = 'RenderMarkdownSuccess' },
+          question = { raw = '[!QUESTION]', rendered = '󰘥 Question', highlight = 'RenderMarkdownWarn' },
+          help = { raw = '[!HELP]', rendered = '󰘥 Help', highlight = 'RenderMarkdownWarn' },
+          faq = { raw = '[!FAQ]', rendered = '󰘥 Faq', highlight = 'RenderMarkdownWarn' },
+          attention = { raw = '[!ATTENTION]', rendered = '󰀪 Attention', highlight = 'RenderMarkdownWarn' },
+          failure = { raw = '[!FAILURE]', rendered = '󰅖 Failure', highlight = 'RenderMarkdownError' },
+          fail = { raw = '[!FAIL]', rendered = '󰅖 Fail', highlight = 'RenderMarkdownError' },
+          missing = { raw = '[!MISSING]', rendered = '󰅖 Missing', highlight = 'RenderMarkdownError' },
+          danger = { raw = '[!DANGER]', rendered = '󱐌 Danger', highlight = 'RenderMarkdownError' },
+          error = { raw = '[!ERROR]', rendered = '󱐌 Error', highlight = 'RenderMarkdownError' },
+          bug = { raw = '[!BUG]', rendered = '󰨰 Bug', highlight = 'RenderMarkdownError' },
+          example = { raw = '[!EXAMPLE]', rendered = '󰉹 Example', highlight = 'RenderMarkdownHint' },
+          quote = { raw = '[!QUOTE]', rendered = '󱆨 Quote', highlight = 'RenderMarkdownQuote' },
+          cite = { raw = '[!CITE]', rendered = '󱆨 Cite', highlight = 'RenderMarkdownQuote' },
+        },
+        link = {
+          enabled = true,
+          footnote = {
+            superscript = true,
+            prefix = '',
+            suffix = '',
+          },
+          image = '󰥶 ',
+          email = '󰀓 ',
+          hyperlink = '󰌹 ',
+          highlight = 'RenderMarkdownLink',
+          wiki = { icon = '󱗖 ', highlight = 'RenderMarkdownWikiLink' },
+          custom = {
+            web = { pattern = '^http', icon = '󰖟 ' },
+          },
+        },
+        sign = {
+          enabled = true,
+          highlight = 'RenderMarkdownSign',
+        },
+        indent = {
+          enabled = false,
+          per_level = 2,
+          skip_level = 1,
+          skip_heading = false,
+        },
+        win_options = {
+          conceallevel = {
+            default = vim.o.conceallevel,
+            rendered = 3,
+          },
+          concealcursor = {
+            default = vim.o.concealcursor,
+            rendered = '',
+          },
+        },
+      })
+    end
+  },
+
   -- Autopair
   {
     'windwp/nvim-autopairs',
