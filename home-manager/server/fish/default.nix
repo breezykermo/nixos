e03323d01a6644862c904390ea09452c88844804
Lachlan Kermode <lachie@ohrg.org>
2026-06-13 13:00:39 +0200

    Confiure to point to local Ollama modesl

--- home-manager/server/fish/default.nix
+++ home-manager/server/fish/default.nix
@@ -24,6 +24,9 @@
       if command -q opam
         eval (opam env)
       end
+
+      # Set Ollama models directory (for homework machine)
+      set -x OLLAMA_MODELS $HOME/data/ollama/models
     '';
 
     plugins = [
